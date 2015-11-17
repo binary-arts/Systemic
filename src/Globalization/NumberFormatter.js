8f2a23021e38bc69b800f880c714d6e82e30ec88
John Pennington <john.pennington@binaryarts.net>
2015-11-17 14:08:34 -0600

    WIP NumberFormatter type; candidate structure for localized formatter data.

--- src/Globalization/NumberFormatter.js
+++ src/Globalization/NumberFormatter.js
@@ -1,115 +1,156 @@
-// import as from '../Runtime/As';
-// import is from '../Runtime/Is';
-//
-// import Culture from './Culture';
-// import Formatter from '../Formatter';
-//
-// export default class NumberFormatter extends Formatter {
-//
-//     //#region Type
-//
-//     //#region Methods
-//
-//     /**
-//      * @private
-//      * @static
-//      *
-//      * @param { Number } ref
-//      * @param { Array<Number> } groupSizes
-//      * @param { String } decimalSeperator
-//      * @param { String } groupSeperator
-//      * @returns { String }
-//      */
-//     static _subFormat(/*ref, groupSizes, decimalSeperator, groupSeperator*/) {
-//         return '';
-//
-//     //     var decimalPart = null;
-//     //     var decimalIndex = number.indexOf(decimal);
-//     //     if (decimalIndex > 0) {
-//     //         decimalPart = number.substr(decimalIndex);
-//     //         number = number.substr(0, decimalIndex);
-//     //     }
-//     //
-//     //     var negative = number.startsWith('-');
-//     //     if (negative) {
-//     //         number = number.substr(1);
-//     //     }
-//     //
-//     //     var groupIndex = 0;
-//     //     var groupSize = groups[groupIndex];
-//     //     if (number.length < groupSize) {
-//     //         return decimalPart ? number + decimalPart : number;
-//     //     }
-//     //
-//     //     var index = number.length;
-//     //     var s = '';
-//     //     var done = false;
-//     //     while (!done) {
-//     //         var length = groupSize;
-//     //         var startIndex = index - length;
-//     //         if (startIndex < 0) {
-//     //             groupSize += startIndex;
-//     //             length += startIndex;
-//     //             startIndex = 0;
-//     //             done = true;
-//     //         }
-//     //         if (!length) {
-//     //             break;
-//     //         }
-//     //
-//     //         var part = number.substr(startIndex, length);
-//     //         if (s.length) {
-//     //             s = part + comma + s;
-//     //         }
-//     //         else {
-//     //             s = part;
-//     //         }
-//     //         index -= length;
-//     //
-//     //         if (groupIndex < groups.length - 1) {
-//     //             groupIndex++;
-//     //             groupSize = groups[groupIndex];
-//     //         }
-//     //     }
-//     //
-//     //     if (negative) {
-//     //         s = '-' + s;
-//     //     }
-//     //     return decimalPart ? s + decimalPart : s;
-//     }
-//
-//     //#endregion
-//
-//     //#endregion
-//
-//     //#region Properties
-//
+import Culture from './Culture';
+import Formatter from '../Formatter';
+
+export default class NumberFormatter extends Formatter {
+
+    //#region Type
+
+    //#region Properties
+
+    /**
+     * A dictionary that contains the collective state of the NumberFormatter type.
+     *
+     * @private
+     * @static
+     *
+     * @returns { Object }
+     */
+    static get _() {
+        return NumberFormatter.__ || (NumberFormatter.__ = Object.create(null));
+    }
+
+    /**
+     * @private
+     * @static
+     *
+     * @returns { Map<Culture, NumberFormatter> }
+     */
+    static get _cache() {
+        return NumberFormatter._.cache || (NumberFormatter._.cache = new Map());
+    }
+
+    /**
+     * @override
+     * @static
+     *
+     * @returns { NumberFormatter }
+     */
+    static get current() {
+        const culture = Culture.current;
+
+        //!!! not thread-safe
+        return NumberFormatter._cache.has(culture)
+            ? NumberFormatter._cache.get(culture)
+            : () => {
+                const result = new NumberFormatter(Culture.current);
+                NumberFormatter._cache.set(culture, result);
+
+                return result;
+            }();
+    }
+
+    //#endregion
+
+    //#region Methods
+
+    /**
+     * @private
+     * @static
+     *
+     * @param { String } ref
+     * @param { String } negativeSign
+     * @param { Array<Number> } groupSizes
+     * @param { String } decimalSeperator
+     * @param { String } groupSeperator
+     * @returns { String }
+     */
+    static _subFormat(ref, negativeSign, groupSizes, decimal, group) {
+        let result = '';
+
+        const isNegative = ref.startsWith(negativeSign);
+
+        if (isNegative)
+            ref = ref.substr(negativeSign.length);
+
+        const decimalIndex = ref.indexOf(decimal);
+        const decimalPart = (decimalIndex > 0) ? ref.substr(decimalIndex) : null;
+
+        if (decimalIndex > 0)
+            ref = ref.substr(0, decimalIndex);
+
+        let groupIndex = 0;
+        let groupSize = groupSizes[groupIndex];
+
+        if (ref.length < groupSize)
+            result = `${ref}${decimalPart}`;
+        else {
+            let index = ref.length;
+            let done = false;
+
+            while (!done) {
+                let length = groupSize;
+                let startIndex = index - length;
+
+                if (startIndex < 0) {
+                    groupSize += startIndex;
+                    length += startIndex;
+                    startIndex = 0;
+                    done = true;
+                }
+
+                if (!length)
+                    break;
+
+                result = `${ref.substr(startIndex, length)}${result ? group : null}${result}`;
+                index -= length;
+
+                if (groupIndex < groupSizes.length - 1) {
+                    groupIndex++;
+                    groupSize = groupSizes[groupIndex];
+                }
+            }
+
+            result = `${result}${decimalPart}`;
+        }
+
+        if (isNegative)
+            result = `${negativeSign}${result}`;
+
+        return result;
+    }
+
+    //#endregion
+
+    //#endregion
+
+    //#region Properties
+
+    /**
+     * @override
+     *
+     * @returns { Array<T> }
+     */
+    get priorityTypes() {
+        return this._.priorityTypes || (this._.priorityTypes = [Number]);
+    }
+
+    //#endregion
+
+    //#region Methods
+
 //     /**
 //      * @override
 //      *
-//      * @returns { Array<T> }
-//      */
-//     get priorityTypes() {
-//         return this._.priorityTypes || (this._.priorityTypes = [Number]);
-//     }
-//
-//     //#endregion
-//
-//     //#region Methods
-//
-//     /**
-//      * @override
-//      *
-//      * @param { * } ref
+//      * @param { Any } ref
 //      * @param { String } spec
-//      * @returns { String? }
+//      * @returns { ?String }
 //      */
 //     format(ref, spec) {
 //         let result = '';
 //
-//         if (is(ref = as(ref).aNumber).defined) {
-//             const culture = Culture.current;
-//             let precision = (spec.length > 1) ? (parseInt(spec.substr(1)) || -1) : -1;
+//         if ((ref = as(ref).aNumber)) {
+//             const culture = this._culture;
+//             const precision = (spec.length > 1) ? (parseInt(spec.substr(1)) || -1) : -1;
 //
 //             switch ((spec = spec.charAt(0))) {
 //                 case 'd':
@@ -184,8 +225,8 @@
 //         }
 //
 //         return result || null;
-//     }
-//
-//     //#endregion
-//
-// }
+//    }
+
+    //#endregion
+
+}
